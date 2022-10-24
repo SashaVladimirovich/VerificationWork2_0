455f211accfb6eaa0e1d9d96fd306f4ef33f00e1
Sahsa <a.v.pavlov09@gmail.com>
2022-10-24 21:56:54 +0300

    Создение метода генерации текстового массива

--- VM2_0/Program.cs
+++ VM2_0/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear(); 
+string[] FillText(string[] text, int wordCount) 
+{ 
+    string librarySymbol = "-:;()*&^%$#@!0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"; 
+ 
+    for (int i = 0; i < wordCount; i++) 
+    { 
+        string word = String.Empty; 
+        int wordLength = new Random().Next(1, 15); 
+        for (int j = 0; j < wordLength; j++) 
+        { 
+            word = word + librarySymbol[new Random().Next(0, 75)]; 
+        } 
+        text[i] = word; 
+    } 
+    return text; 
+} 
