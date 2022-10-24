2c566378ed3b8f585aa5fbded5fbcb79857b679d
Sahsa <a.v.pavlov09@gmail.com>
2022-10-24 22:00:38 +0300

    Готовая программа генерации текстового массива и удаление из него слов длинной более 3х символов

--- VM2_0/Program.cs
+++ VM2_0/Program.cs
@@ -1,37 +1,44 @@
-﻿Console.Clear(); 
-string[] FillText(string[] text, int wordCount) 
-{ 
-    string librarySymbol = "-:;()*&^%$#@!0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"; 
- 
-    for (int i = 0; i < wordCount; i++) 
-    { 
-        string word = String.Empty; 
-        int wordLength = new Random().Next(1, 15); 
-        for (int j = 0; j < wordLength; j++) 
-        { 
-            word = word + librarySymbol[new Random().Next(0, 75)]; 
-        } 
-        text[i] = word; 
-    } 
-    return text; 
-} 
-string PrintText(string[] text) 
-{ 
-    return  String.Join(' ', text); 
-} 
-string DelWord(string[] text) 
-{ 
-    string newText = string.Empty; 
-    for (int index = 0; index < text.Length; index++) 
-    { 
-        string newWord = text[index]; 
-        if (newWord.Length <= 3) 
-        { 
-            newText += newWord + " "; 
-        } 
- 
-    } 
-    return newText; 
-} 
+﻿Console.Clear();
+string[] FillText(string[] text, int wordCount)
+{
+    string librarySymbol = "-:;()*&^%$#@!0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    for (int i = 0; i < wordCount; i++)
+    {
+        string word = String.Empty;
+        int wordLength = new Random().Next(1, 15);
+        for (int j = 0; j < wordLength; j++)
+        {
+            word = word + librarySymbol[new Random().Next(0, 75)];
+        }
+        text[i] = word;
+    }
+    return text;
+}
+string PrintText(string[] text)
+{
+    return String.Join(' ', text);
+}
+string DelWord(string[] text)
+{
+    string newText = string.Empty;
+    for (int index = 0; index < text.Length; index++)
+    {
+        string newWord = text[index];
+        if (newWord.Length <= 3)
+        {
+            newText += newWord + " ";
+        }
+
+    }
+    return newText;
+}
+int wordCount = new Random().Next(2, 30);
+string[] text = new string[wordCount];
+text = FillText(text, wordCount);
+Console.WriteLine(PrintText(text));
+Console.WriteLine();
+string newText = DelWord(text);
+Console.WriteLine($"{newText}");
 
 
